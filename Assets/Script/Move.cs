813c1f26c28a2daba8c4e1b3118beb775246a0fb
Nikita <89220602+VoRaX00@users.noreply.github.com>
2022-12-24 19:44:03 +0500

    Начало

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float jumpForce = 15f; //сила прыжка
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        // Vector3 dir = transform.up * Input.GetAxis("Jump");
+        //transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, jumpForce);
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.8f);
+        isGrounded = collider.Length > 1;
+
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        else if (isGrounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+}
