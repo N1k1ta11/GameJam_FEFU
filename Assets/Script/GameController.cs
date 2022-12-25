41ab618c0d4795499858968d2a27b7653a6efe2e
Nikita <89220602+VoRaX00@users.noreply.github.com>
2022-12-25 10:57:18 +0500

    появились все скрипты

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private Vector3 pos;
+
+    private void Awake()
+    {
+        if (!_player)
+        {
+            _player = FindObjectOfType<Move>().transform;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = _player.position;
+        pos.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+    }
+}
